04b1623dc5fb2f6de622b81b1609b416f401993b
iamnovinfar.ir <amirnovin80@gmail.com>
2022-03-22 13:47:00 +0430

    complete First Part of Application

--- app/src/main/java/ir/iamnovinfar/Shorten_link/ToolsActivity.java
+++ app/src/main/java/ir/iamnovinfar/Shorten_link/ToolsActivity.java
@@ -7,8 +7,10 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -16,6 +18,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ToolsActivity extends AppCompatActivity {
     TextView shorten_txt_url;
@@ -67,8 +77,22 @@ public class ToolsActivity extends AppCompatActivity {
                 Toast.makeText(ToolsActivity.this, "as", Toast.LENGTH_SHORT).show();
             }
         });
-        Toast.makeText(ToolsActivity.this, "asss", Toast.LENGTH_SHORT).show();
 
+        generate_qrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.encodeBitmap(link, BarcodeFormat.QR_CODE, 400, 400);
+                    ImageView imageViewQrCode = (ImageView) findViewById(R.id.imageview_qrcode);
+                    imageViewQrCode.setImageBitmap(bitmap);
+
+                    SaveImage(bitmap,getApplicationContext());
+                } catch(Exception e) {
+
+                }
+            }
+        });
     }
 
 
@@ -85,4 +109,31 @@ public class ToolsActivity extends AppCompatActivity {
 
 
     }
+
+    private static void SaveImage(Bitmap finalBitmap,Context context) {
+
+        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File myDir = new File(root + "/LinkShortener");
+        myDir.mkdirs();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.US);
+        Date now = new Date();
+        String time = formatter.format(now);
+
+        String fname = time +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+            Toast.makeText(context, "بارکد در حافظه شما ذخیره شد...", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
