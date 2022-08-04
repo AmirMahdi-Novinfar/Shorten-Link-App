c9e63d628cdf58bcfb065ed4101ce2a2dbb74cf1
AmirMahdi NovinFar HOME <amirnovin80@gmail.com>
2022-08-05 01:29:34 +0430

    Strating version 2 in app

---
+++ app/src/main/java/ir/iamnovinfar/Shorten_link/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package ir.iamnovinfar.Shorten_link.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.iamnovinfar.Shorten_link.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/ir/iamnovinfar/Shorten_link/Activity/SplashActivity.java
+++ app/src/main/java/ir/iamnovinfar/Shorten_link/Activity/SplashActivity.java
@@ -2,11 +2,16 @@ package ir.iamnovinfar.Shorten_link.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
 
+import com.aghajari.rlottie.AXrLottie;
+import com.aghajari.rlottie.AXrLottieDrawable;
+import com.aghajari.rlottie.AXrLottieImageView;
 
 import ir.iamnovinfar.Shorten_link.R;
 
@@ -18,13 +23,30 @@ public class SplashActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        setUpAnimation();
 
-//        AXrLottie.init(this);
-//
-//        AXrLottieImageView lottieView=findViewById(R.id.lottie_view);
-//        lottieView.setLottieDrawable(AXrLottieDrawable.fromAssets(this,"loading.json")
-//                .setSize(100,100)
-//                .build());
-//        lottieView.playAnimation();
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        },3000);
+
+
+    }
+
+
+
+
+    private void setUpAnimation(){
+        AXrLottie.init(this);
+
+        AXrLottieImageView lottieView=findViewById(R.id.lottie_view);
+        lottieView.setLottieDrawable(AXrLottieDrawable.fromAssets(this,"loading2.json")
+                .setSize(500,500)
+                .build());
+        lottieView.playAnimation();
     }
 }
